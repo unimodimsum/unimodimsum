2f339793e39d130afec00166dbe12a0b13bc901a
unimodimsum <unimodimsum@gmail.com>
2026-01-14 11:10:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  * @param {string} product - Nama produk yang ingin dipesan
  */
 function orderWA(product) {
-    const phoneNumber = "882009549480"; // GANTI DENGAN NOMOR WHATSAPP ANDA
+    const phoneNumber = "+62882009549480"; // GANTI DENGAN NOMOR WHATSAPP ANDA
     let message = "";
 
     if (product === 'Halo' || product === 'Tanya') {
